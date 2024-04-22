4084c7d1cf78794c7f7cc24330e459730e72c1ce
jutranjo <pacnik@gmail.com>
2024-04-22 15:54:13 +0200

    Wrote some tests and functions, no struct, partially through chapter

---
+++ structs_methods/struct_methods.go
@@ -0,0 +1,11 @@
+package main
+
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
+
+func Area(width float64, height float64) float64 {
+	return width * height
+}
+
+//up to https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/structs-methods-and-interfaces#refactor
